04355b73845c92e0027d2b0f4e2fb37e8579d16f
itarundanielgithub <79411896+itarundanielgithub@users.noreply.github.com>
2023-06-26 11:37:41 -0400

    Update unusedEIPs.sh

--- unusedEIPs.sh
+++ unusedEIPs.sh
@@ -4,11 +4,15 @@
 for region in $(aws ec2 describe-regions --output text --query 'Regions[].RegionName')
 do
   echo "Checking region $region"
-  
-  # Fetch all Elastic IPs
-  for allocation in $(aws ec2 describe-addresses --region "$region" --query "Addresses[?InstanceId==null].AllocationId" --output text)
+
+  # Fetch all Elastic IPs not allocated to a network interface
+  for allocation in $(aws ec2 describe-addresses --region "$region" --query "Addresses[?AssociationId==null].AllocationId" --output text)
   do
     echo "Releasing unattached Elastic IP with allocation id $allocation in region $region"
     aws ec2 release-address --region "$region" --allocation-id "$allocation"
   done
+
+  # Add other services and their corresponding commands here
+  # ...
+
 done
