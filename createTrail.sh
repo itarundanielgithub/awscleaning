cbeb46db7463c7772182885df30bebad91480ff2
Arun Daniel <arudani@cdw.com>
2023-08-18 08:59:37 -0400

    second comm2

--- createTrail.sh
+++ createTrail.sh
@@ -1,25 +1,55 @@
-aws logs create-log-group --log-group-name 'arun-logs'
-aws s3 mb s3://arun0213b
-echo '{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AWSCloudTrailWrite",
-            "Effect": "Allow",
-            "Principal": {
-              "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "s3:*",
-            "Resource": ["arn:aws:s3:::arun0213b", "arn:aws:s3:::arun0213b/*"]
-        }
-    ]
-}' > bucketpolicy.json
-aws s3api put-bucket-policy --bucket arun0213b --policy file://bucketpolicy.json
-echo '{
+#!/bin/bash
+
+
+# Get the AWS account ID
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text --region us-east-1)
+
+# Define trail, log group, and bucket names
+TRAIL_NAME="arun3MyCloudTrail"
+CLOUDWATCH_LOG_GROUP_NAME="arun3MyCloudTrailLogGroup"
+ROLE_NAME="arun3CloudTrail_CW_Role"
+POLICY_NAME="arun3CloudTrail_CW_Policy"
+S3_BUCKET_NAME="arun3my-cloudtrail-bucket-$ACCOUNT_ID-$(date +%s)"
+
+# Create an S3 bucket for CloudTrail logs
+aws s3api create-bucket --bucket $S3_BUCKET_NAME --region us-east-1
+
+# Set the S3 bucket policy to allow CloudTrail to write to it
+aws s3api put-bucket-policy --bucket $S3_BUCKET_NAME --policy '{
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "",
+      "Sid": "AWSCloudTrailAclCheck20150319",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Action": "s3:GetBucketAcl",
+      "Resource": "arn:aws:s3:::'$S3_BUCKET_NAME'"
+    },
+    {
+      "Sid": "AWSCloudTrailWrite20150319",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::'$S3_BUCKET_NAME'/AWSLogs/'$ACCOUNT_ID'/*",
+      "Condition": {
+        "StringEquals": {
+          "s3:x-amz-acl": "bucket-owner-full-control"
+        }
+      }
+    }
+  ]
+}' --region us-east-1
+
+
+# Create Role for CloudTrail to CloudWatch
+aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document '{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
       "Effect": "Allow",
       "Principal": {
         "Service": "cloudtrail.amazonaws.com"
@@ -27,25 +57,42 @@ echo '{
       "Action": "sts:AssumeRole"
     }
   ]
-}' > trailrole.json
-aws iam create-role --role-name trailrole --assume-role-policy-document file://trailrole.json
-echo '{
+}' --region us-east-1
+
+# Attach policies to the role to allow CloudTrail to publish to CloudWatch Logs
+aws iam put-role-policy --role-name $ROLE_NAME --policy-name $POLICY_NAME --policy-document '{
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": [
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
       "Effect": "Allow",
-      "Resource": "*"
+      "Action": [
+        "logs:PutLogEvents",
+        "logs:CreateLogStream"
+      ],
+      "Resource": [
+
+        	"arn:aws:logs:us-east-1:'$ACCOUNT_ID':log-group:'$CLOUDWATCH_LOG_GROUP_NAME':*"
+
+      ]
     }
   ]
-}' > trailpolicy.json
-aws iam create-policy --policy-name trailpolicy --policy-document file://trailpolicy.json
-trailpolicy_arn=$(aws iam list-policies --query 'Policies[?PolicyName==`trailpolicy`].Arn' --output text)
-aws iam attach-role-policy --role-name trailrole --policy-arn $trailpolicy_arn
-trailrole_arn=$(aws iam get-role --role-name trailrole --query 'Role.Arn' --output text)
-loggroup_arn=$(aws logs describe-log-groups --log-group-name-prefix arun-logs --query 'logGroups[0].arn' --output text)
-aws cloudtrail create-trail --name arun0213cloudtrail --s3-bucket-name arun0213b --no-is-multi-region-trail --cloud-watch-logs-role-arn $trailrole_arn --cloud-watch-logs-log-group-arn $loggroup_arn
-aws cloudtrail start-logging --name arun0213cloudtrail
+}' --region us-east-1
+
+# Create CloudWatch Logs log group
+aws logs create-log-group --log-group-name $CLOUDWATCH_LOG_GROUP_NAME --region us-east-1
+
+sleep 10
+
+# Create CloudTrail with integration to CloudWatch Logs
+aws cloudtrail create-trail --name $TRAIL_NAME \
+  --s3-bucket-name $S3_BUCKET_NAME \
+  --include-global-service-events \
+  --is-multi-region-trail \
+  --cloud-watch-logs-log-group-arn "arn:aws:logs:us-east-1:$ACCOUNT_ID:log-group:$CLOUDWATCH_LOG_GROUP_NAME:*" \
+  --cloud-watch-logs-role-arn "arn:aws:iam::$ACCOUNT_ID:role/$ROLE_NAME" \
+  --region us-east-1
+
+# Start logging
+aws cloudtrail start-logging --name $TRAIL_NAME --region us-east-1
+
+echo "CloudTrail created and logging started!"
